00cdcff87bbf7cc4bb19a55196c9e6e5826c7f53
KamruzzamanAsif <asif720kamruzzaman@gmail.com>
2023-09-04 22:41:12 +0600

    rook moves added

--- Mini_Chess/engine.py
+++ Mini_Chess/engine.py
@@ -92,7 +92,27 @@ class GameState():
     '''
 
     def getRookMoves(self, r, c, moves):
-        pass
+        directions = ((-1, 0), (0, -1), (1, 0), (0, 1)) # up, left, down right
+        enemyColor = 'b' if self.whiteToMove else 'w'
+
+        for d in directions:
+            for i in range(1, 6):
+                endRow = r + d[0]*i
+                endCol = c + d[1]*i
+
+                if 0<= endRow < 6 and 0<= endCol < 5:   # check on board
+                    endPiece = self.board[endRow][endCol]
+                    if endPiece == '--': # empty space so valid
+                        moves.append(Move((r,c), (endRow, endCol), self.board))
+                    elif endPiece[0] == enemyColor:
+                        moves.append(Move((r,c), (endRow, endCol), self.board))
+                        break
+                    else: # friendly piece (own piece)
+                        break
+                else: # off board
+                    break 
+
+                        
 
 
     '''
