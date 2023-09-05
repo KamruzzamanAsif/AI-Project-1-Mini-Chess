ef416d91c7159be59fe32b936b7cb64d42ffd8fb
KamruzzamanAsif <asif720kamruzzaman@gmail.com>
2023-09-05 08:16:03 +0600

    moves added concrening checks| at the end checkmate and stalemate checking not done yet

--- Mini_Chess/engine.py
+++ Mini_Chess/engine.py
@@ -18,12 +18,26 @@ class GameState():
         self.whiteToMove = True
         self.moveLog = []
 
+        # king location
+        self.whiteKingLocation = (5,4)
+        self.blackKingLocation = (0,4)
+
+        # checkmate and stalemate
+        self.checkMate = False
+        self.staleMate = False
+
     # makes moves except castling, en passant, pawn promotion
     def makeMove(self, move):
         self.board[move.startRow][move.startCol] = "--"
         self.board[move.endRow][move.endCol] = move.pieceMoved
         self.moveLog.append(move)
         self.whiteToMove = not self.whiteToMove
+        # update the king's loaction if moved
+        if move.pieceMoved == 'b_K':
+            self.blackKingLocation = (move.endRow, move.endCol)
+        elif move.pieceMoved == 'w_K':
+            self.whiteKingLocation = (move.endRow, move.endCol)
+
 
     def undoMove(self, mode='single'):
         if mode == 'all':
@@ -32,19 +46,76 @@ class GameState():
                 self.board[move.startRow][move.startCol] = move.pieceMoved
                 self.board[move.endRow][move.endCol] = move.pieceCaptured
                 self.whiteToMove = not self.whiteToMove
+            # reset the king's location to inital after undo all
+            # this means a new game
+            # king location
+            self.whiteKingLocation = (5,4)
+            self.blackKingLocation = (0,4)
 
         elif len(self.moveLog) != 0:
             move = self.moveLog.pop()
             self.board[move.startRow][move.startCol] = move.pieceMoved
             self.board[move.endRow][move.endCol] = move.pieceCaptured
-            self.whiteToMove = not self.whiteToMove
+            self.whiteToMove = not self.whiteToMove 
+            # update the king's loaction if moved
+            if move.pieceMoved == 'b_K':
+                self.blackKingLocation = (move.startRow, move.startCol)
+            elif move.pieceMoved == 'w_K':
+                self.whiteKingLocation = (move.startRow, move.startCol)
+
+
+
+
 
     # >>>>>>>>>>>>   Move Generation Section <<<<<<<<<<<<<<<<<<
 
     # all moves with considering checks
 
     def getValidMoves(self):
-        return self.getAllPossibleMoves()
+        # >>> the following algorithm is naive and 
+        # is implemented concerning the checks <<<
+
+        # step-1: generate all possible moves
+        moves = self.getAllPossibleMoves()
+
+        # step-2: for each move make the move
+        for i in range(len(moves)-1, -1, -1): # when removing from list we go backward
+            self.makeMove(moves[i])
+            # step-3: generate all opponent's move
+            # step-4: for each of opponent's move, see if the attact your king
+            self.whiteToMove = not self.whiteToMove
+            if self.inCheck():
+                moves.remove(moves[i]) # step-5: if they attack your king, it's not a valid move
+            self.whiteToMove = not self.whiteToMove
+            self.undoMove()
+        
+
+        # part-7 of video
+        # TODO: "checkmate and stalemate check kora baki"
+
+        return moves
+
+
+
+
+    '''Determine if the current player in check'''
+    def inCheck(self):
+        if self.whiteToMove:
+            return self.squareUnderAttack(self.whiteKingLocation[0], self.whiteKingLocation[1])
+        else:
+            return self.squareUnderAttack(self.blackKingLocation[0], self.blackKingLocation[1])
+
+    '''Determine if the enemy can attack the square(r, c)'''
+    def squareUnderAttack(self, r, c):
+        self.whiteToMove = not self.whiteToMove # switch to opponent's move
+        oppMoves = self.getAllPossibleMoves()
+        self.whiteToMove = not self.whiteToMove # switch turns back
+        for move in oppMoves:
+            if move.endRow == r and move.endCol == c: # square under attack
+                return True
+        return False
+
+
 
     # all moves without considering checks
     def getAllPossibleMoves(self):
